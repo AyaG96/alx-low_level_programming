3c7e27076f5a8061b4e21b588162bee15c0c22a0
AyaG96 <aya.gamal.zaher@gmail.com>
2023-09-07 13:44:20 -0700

    new files added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - entry point
+ * return: o means success
+ *
+ * /
 int main(void)
-{ 
+{
 
-puts("Programming is like building a multilingual puzzle\n");
-return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
 
 }
 
