836a40ad923961086d76c0e904a75d0fac6df99c
AyaG96 <aya.gamal.zaher@gmail.com>
2023-09-09 11:22:43 -0700

    new files added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - display numbers from 0 to 9
+ * Return: return 0 and exit program.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+
+	printf("\n");
+	return (0);
+}
