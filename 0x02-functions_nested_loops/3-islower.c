73e8f2c4f32ac435b32de381d69e26bc309753e8
AyaG96 <aya.gamal.zaher@gmail.com>
2023-09-13 12:34:06 -0700

    new files added

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Checks for lowercase character
- * 
+ *
  * @c: The character to be checked
  *
  * Return: 1 if c is lowercase, 0 otherwise
@@ -11,7 +11,7 @@ int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	else	
+	else
 		return (0);
 
 }
