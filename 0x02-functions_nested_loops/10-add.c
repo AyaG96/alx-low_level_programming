38ec038fe3a52701378fa5dfffd96536d4f2695e
AyaG96 <aya.gamal.zaher@gmail.com>
2023-09-13 15:52:37 -0700

    new filea added

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,12 @@
 
 /**
  * add - Adds two integers
+ * @a: The first integer
+ * @b: The second integer
  *
  * Return: The sum of the two integers
  */
-int add(int, int);
+int add(int a, int b)
 {
-	int a, b;
-
 	return (a + b);
 }
