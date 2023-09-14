e837f618f2115c0093cc42c145538e4928102e3c
AyaG96 <aya.gamal.zaher@gmail.com>
2023-09-14 15:15:00 -0700

    new files added

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check code.
+ *
+ *@n: variable
+ *
+ * Return: Always
+ */
+void print_diagonal(int n)
+{
+	int num, s;
+
+	if (n <= 0)
+
+		_putchar('\n');
+	else
+	{
+		for (num = 1; num <= n; num++)
+		{
+			for (s = 1; s <= num; s++)
+			{
+				_putchar(' ');
+			}
+			_putchar('/');
+			_putchar('\n');
+		}
+	}
+
+
+
+}
