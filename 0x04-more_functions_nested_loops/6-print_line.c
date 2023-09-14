a3d63142099780a4f55b769102812f6a980c7faf
AyaG96 <aya.gamal.zaher@gmail.com>
2023-09-14 13:32:14 -0700

    new files added

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - check code.
+ *
+ *@n: variable
+ *
+ * Return: Always
+ */
+
+void print_line(int n)
+{
+	int num;
+
+	if (n > 0)
+	{
+		for (num = 1; num <= n; num++)
+		{
+			_putchar('-');
+		}
+		_putchar('\n');
+	}
+	
+	else
+	_putchar('\n');
+}
