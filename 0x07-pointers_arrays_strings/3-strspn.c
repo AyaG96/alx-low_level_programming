3cd66af6db6172121e7923e1b695f92d4a931be5
AyaG96 <aya.gamal.zaher@gmail.com>
2023-09-26 15:36:11 -0700

    added

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,6 +11,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, count;
+
 	count = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
@@ -23,8 +24,8 @@ unsigned int _strspn(char *s, char *accept)
 			}
 			if (s[i] == ' ')
 			{
-			return (count);
-			}	
+				return (count);
+			}
 		}
 	}
 	return (count);
