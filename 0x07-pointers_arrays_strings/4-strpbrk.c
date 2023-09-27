09a8615358d76547d09530be94e8b4b95370235c
AyaG96 <aya.gamal.zaher@gmail.com>
2023-09-27 09:03:20 -0700

    added

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,16 +11,14 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
-	char *t;
 
-	t = &s[i];
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				return (t);
+				return (&s[i]);
 			}
 		}
 	}
