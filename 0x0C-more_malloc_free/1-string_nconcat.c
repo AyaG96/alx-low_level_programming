50bf449e60fca7a5e237308e643027d06ca64571
AyaG96 <aya.gamal.zaher@gmail.com>
2023-10-07 13:28:56 -0700

    added

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (j = 0; j < n && s2[j] != '\0'; j++)
 		;
-	s3 = malloc((i + j + 1) * sizeof(char));
+	s3 = malloc((i + j) * sizeof(char));
 
 	if (s3 == NULL)
 		return (NULL);
