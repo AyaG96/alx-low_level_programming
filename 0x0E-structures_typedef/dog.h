927e5aa5ade9d795b6e5ad9bfb8641546501ce24
AyaG96 <aya.gamal.zaher@gmail.com>
2023-10-11 15:10:46 -0700

    added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,12 +13,14 @@
  * description: dog struct
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
